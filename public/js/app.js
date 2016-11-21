c7bcfadf1c6af1376e5a152cbdfcec69eee6c375
ha10id <ha10id@cit-sk.ru>
2016-11-21 14:05:32 +0300

    ed

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,28 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap', 'myApp.filters', 'myApp.services', 'myApp.directives']).
-config(["$provide", function($provide){
-  // Use the `decorator` solution to substitute or attach behaviors to
-  // original service instance; @see angular-mocks for more examples....
-  $provide.decorator( '$log', [ "$delegate", function( $delegate )
-  {
-      // Save the original $log.debug()
-      var debugFn = $delegate.debug;
+// config(["$provide", function($provide){
+//   // Use the `decorator` solution to substitute or attach behaviors to
+//   // original service instance; @see angular-mocks for more examples....
+//   $provide.decorator( '$log', [ "$delegate", function( $delegate )
+//   {
+//       // Save the original $log.debug()
+//       var debugFn = $delegate.debug;
 
-      $delegate.debug = function( )
-      {
-        var args    = [].slice.call(arguments),
-            now     = new Date();
+//       $delegate.debug = function( )
+//       {
+//         var args    = [].slice.call(arguments),
+//             now     = new Date();
 
-        // Prepend timestamp
-        args[0] = supplant("{0} - {1}", [ now, args[0] ]);
+//         // Prepend timestamp
+//         args[0] = supplant("{0} - {1}", [ now, args[0] ]);
 
-        // Call the original with the output prepended with formatted timestamp
-        debugFn.apply(null, args)
-      };
+//         // Call the original with the output prepended with formatted timestamp
+//         debugFn.apply(null, args)
+//       };
 
-      return $delegate;
-  }])
-}]).
+//       return $delegate;
+//   }])
+// }]).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider.
   when('/', {
